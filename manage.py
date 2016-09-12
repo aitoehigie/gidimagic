288c6422eabd02cc6dbfdb64d720397577987cdf
aitoehigie <aitoehigie@gmail.com>
2016-09-12 01:45:55 +0100

    g

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-
-from flask_script import Manager
 from app import app
+#from flask_script import Manager
+
 
 #manager = Manager(app)
 
-#if __name__ == "__main__":
-#    manager.run()
+if __name__ == "__main__":
+    app.run()
