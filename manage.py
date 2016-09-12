b1cb35799d4a1978ffdb239c8ab367f50853f93a
aitoehigie <aitoehigie@gmail.com>
2016-09-12 01:51:17 +0100

    lost

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from app import app
-#from flask_script import Manager
+from flask_script import Manager
 
 
-#manager = Manager(app)
+manager = Manager(app)
 
 if __name__ == "__main__":
-    app.run()
+    manager.run()
